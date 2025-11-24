359849ed04fe7896ddcecfce49b004884b8f23dc
Bohemian-Ophidian <Karan_whistleblower@protonmail.com>
2025-11-24 20:42:14 +0530

    updated the main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
 #include "sensors.h"
 #include "webserver.h"
+#include "Blynk_server.h"
 
 
 void setup(){
     Serial.begin(115200);
 
     sensorsInit();
-    startWebServer();
+    startup();
+
 }
 
 void loop() {
     readSensors();
     delay(2000);
+    run_code();
 }
