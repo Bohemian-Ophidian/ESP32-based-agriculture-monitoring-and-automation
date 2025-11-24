ebdeba8b12820ea3ed5e3c414a2dad6a20f9a880
Bohemian-Ophidian <Karan_whistleblower@protonmail.com>
2025-11-24 11:37:42 +0530

    Blynk server implementation

---
+++ src/Blynk_server.cpp
@@ -0,0 +1,12 @@
+#define BLYNK_TEMPLATE_ID "SOMETHING_SOMETHING"
+#define BLYNK_TEMPLATE_NAME "SOMETHING_SOMETHING2"
+#define BLYNK_AUTH_TOKEN "SOMETHING_SOMETHING_AGAIN"
+
+#include <WiFi.h>
+#include <Arduino.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char ssid[] = "your_ssid";
+char password[] = "your_password";
+
